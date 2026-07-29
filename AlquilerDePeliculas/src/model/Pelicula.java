a83f86e943db1f8254cb445dbd1491a42a96a989
Srjorgeluis01 <jorge.more.ramos@gmail.com>
2026-07-29 12:47:18 -0500

    Agregar atributos y métodos a Pelicula

--- AlquilerDePeliculas/src/model/Pelicula.java
+++ AlquilerDePeliculas/src/model/Pelicula.java
@@ -9,5 +9,35 @@ package model;
  * @author Jorge Luis
  */
 public class Pelicula {
-    
+    private int id;
+    private String titulo;
+    private String genero;
+    private double precio;
+
+    public Pelicula() {}
+
+    public Pelicula(String titulo, String genero, double precio) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.precio = precio;
+    }
+
+    public Pelicula(int id, String titulo, String genero, double precio) {
+        this(titulo, genero, precio);
+        this.id = id;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getGenero() { return genero; }
+    public void setGenero(String genero) { this.genero = genero; }
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    @Override
+    public String toString() {
+        return id + " - " + titulo;
+    }
 }
