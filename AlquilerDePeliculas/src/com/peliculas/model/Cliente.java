56943bbdc6859edc004ac4ca2f871d4edeb260a2
Angel <tu_correo@ejemplo.com>
2026-07-29 16:44:43 -0500

    feat: Se crea el modelo Cliente con sus atributos

---
+++ AlquilerDePeliculas/src/com/peliculas/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.peliculas.model;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String dni;
+    private String telefono;
+
+    public Cliente() {}
+
+    public Cliente(String nombre, String dni, String telefono) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = telefono;
+    }
+
+    public Cliente(int id, String nombre, String dni, String telefono) {
+        this(nombre, dni, telefono);
+        this.id = id;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    @Override
+    public String toString() {
+        return nombre + " (DNI: " + dni + ")";
+    }
+}
